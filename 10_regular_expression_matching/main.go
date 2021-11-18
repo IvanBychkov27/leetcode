baf791bc95718f11d32ae9878bc8193b0f38c7df
Ivan <IvanBychkov@mail.ru>
2021-11-18 19:32:27 +0300

    Добавлена функция 10: regular-expression-matching

---
+++ 10_regular_expression_matching/main.go
@@ -0,0 +1,120 @@
+// https://leetcode.com/problems/regular-expression-matching/
+/*
+Учитывая входную строку s и шаблон p, реализуйте сопоставление регулярных выражений с поддержкой '.' и '*', где:
+"." Соответствует любому отдельному символу.
+'*' Соответствует нулю или более предыдущего элемента.
+Совпадение должно охватывать всю входную строку (не частично).
+
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" не соответствует всей строке "aa".
+
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' означает ноль или более предыдущего элемента, 'a'. Поэтому, повторяя "а" один раз, оно становится "аа".
+
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Input: s = "aab", p = "c*a*b"
+Output: true
+Explanation: c - м.б. повторено 0 раз, а - м.б. повторено 1 раз. Следовательно, соответствует "aab".
+
+Input: s = "mississippi", p = "mis*is*p*."
+Output: false
+*/
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	//s, p := "aab", "c*a*b" // true
+	//s, p := "ab", ".*" // true
+	//s, p := "aa", "a*" // true
+	//s, p := "aa", "a" // false
+	//s, p := "ab", ".*c" // false
+	//s, p := "aaa", "ab*a*c*a" // true
+	//s, p := "mississipp", "mis*is*ip*" // true
+	//s, p := "mississippi", "mis*is*ip*." // true
+	//s, p := "i", "." // true
+	//s, p := "aaa", "aaaa" // false
+	s, p := "aaba", "ab*a*c*a" // false
+
+	res := isMatch(s, p)
+	fmt.Println("res  =", res)
+}
+
+func isMatch(s string, p string) bool {
+	var idxD, idxS int
+	var predD, predS byte
+	data := []byte(s)
+	shablon := []byte(p)
+	lenD := len(data)
+	lenS := len(shablon)
+
+	if !bytes.Contains(shablon, []byte(".")) && !bytes.Contains(shablon, []byte("*")) {
+		return s == p
+	}
+
+	for {
+		if idxD >= lenD {
+			if data[lenD-1] != shablon[lenS-1] && shablon[lenS-1] != '*' && shablon[lenS-1] != '.' {
+				return false
+			}
+			break
+		}
+		if idxS >= lenS {
+			return false
+		}
+		if idxD > 0 {
+			predD = data[idxD]
+		}
+		if idxS > 0 {
+			predS = shablon[idxS]
+		}
+
+		if data[idxD] != shablon[idxS] && shablon[idxS] == '.' {
+			idxD++
+			idxS++
+			continue
+		}
+
+		if data[idxD] != shablon[idxS] && shablon[idxS] != '.' && shablon[idxS] != '*' && idxS < lenS-1 && shablon[idxS+1] != '*' {
+			return false
+		}
+
+		if data[idxD] == shablon[idxS] {
+			idxD++
+			idxS++
+			continue
+		}
+
+		if data[idxD] != shablon[idxS] && idxS < lenS-1 && shablon[idxS+1] == '*' && shablon[idxS] != '.' {
+			idxS += 2
+			continue
+		}
+
+		if data[idxD] != shablon[idxS] && shablon[idxS] == '*' {
+			if data[idxD] != predD && predS != '.' {
+				return false
+			}
+			//if data[idxD-1] == shablon[idxS-1] {
+			//	idxS++
+			//	continue
+			//}
+			idxD++
+			idxS++
+			continue
+		}
+
+		idxD++
+		idxS++
+
+	}
+
+	return true
+}
---
+++ 10_regular_expression_matching/main_test.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_isMatch(t *testing.T) {
+	type args struct {
+		s string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "01",
+			args: args{
+				s: "aab",
+				p: "c*a*b",
+			},
+			want: true,
+		},
+		{
+			name: "02",
+			args: args{
+				s: "ab",
+				p: ".*",
+			},
+			want: true,
+		},
+		{
+			name: "03",
+			args: args{
+				s: "aa",
+				p: "a*",
+			},
+			want: true,
+		},
+		{
+			name: "04",
+			args: args{
+				s: "aa",
+				p: "a",
+			},
+			want: false,
+		},
+		{
+			name: "05",
+			args: args{
+				s: "ab",
+				p: ".*c",
+			},
+			want: false,
+		},
+		{
+			name: "06",
+			args: args{
+				s: "aaa",
+				p: "ab*a*c*a",
+			},
+			want: true,
+		},
+		{
+			name: "07",
+			args: args{
+				s: "mississipp",
+				p: "mis*is*ip*",
+			},
+			want: true,
+		},
+		{
+			name: "08",
+			args: args{
+				s: "mississippi",
+				p: "mis*is*ip*.",
+			},
+			want: true,
+		},
+		{
+			name: "09",
+			args: args{
+				s: "i",
+				p: ".",
+			},
+			want: true,
+		},
+		{
+			name: "10",
+			args: args{
+				s: "aaa",
+				p: "aaa",
+			},
+			want: true,
+		},
+		{
+			name: "11",
+			args: args{
+				s: "aaa",
+				p: "aaaa",
+			},
+			want: false,
+		},
+		//{
+		//	name: "12",
+		//	args: args{
+		//		s: "aaba",
+		//		p: "ab*a*c*a",
+		//	},
+		//	want: false,
+		//},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isMatch(tt.args.s, tt.args.p); got != tt.want {
+				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
