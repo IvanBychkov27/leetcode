4a21dcfc92b657821a517c5fe6ec82a385f6d116
Ivan <IvanBychkov@mail.ru>
2021-12-08 13:29:04 +0300

    Добавлена функция 19:

---
+++ 19_remove_nth_node_from_end_of_list/main.go
@@ -0,0 +1,31 @@
+/* https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+Удалить N-Й узел Из Конца списка
+Учитывая заголовок связанного списка, удалите n-й узел из конца списка и верните его заголовок.
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Input: head = [1], n = 1
+Output: []
+
+*/
+
+package main
+
+// определение односвязного списка
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+	return nil
+}
