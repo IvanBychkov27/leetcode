fc07b9970006e23706fad74cf879fdaff22f4c2c
Ivan <IvanBychkov@mail.ru>
2022-09-14 11:18:40 +0300

    Add: добавлены тесты для быстрого возведения числа в степень

--- data_algorithms/rapid_exponentiation/rapid_exponentiation.go
+++ data_algorithms/rapid_exponentiation/rapid_exponentiation.go
@@ -6,10 +6,16 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("rapid =", rapidExponentiation(2, 10))
+	a, n := 2, 62
+	fmt.Println("rapid  =", rapidExponentiation(a, n))
+	fmt.Println("rapid2 =", rapidExponentiation2(a, n))
+	fmt.Println("pow    =", int(math.Pow(float64(a), float64(n))))
 }
 
 /* быстрое возведение в степень
@@ -29,4 +35,23 @@ func rapidExponentiation(a, n int) int {
 		res = a * rapidExponentiation(a, n-1)
 	}
 	return res
+}
+
+/* быстрое возведение в степень
+    1, при n = 0
+    a*a^(n-1), при n - не четное
+   (a*a)^(n/2), при n - четное
+   (a*a*a)^(n/3), при n делящееся на 3 без остатка
+*/
+func rapidExponentiation2(a, n int) int {
+	if n == 0 {
+		return 1
+	}
+	if n%3 == 0 {
+		return rapidExponentiation(a*a*a, n/3)
+	}
+	if n%2 == 0 {
+		return rapidExponentiation(a*a, n/2)
+	}
+	return a * rapidExponentiation(a, n-1)
 }
---
+++ data_algorithms/rapid_exponentiation/rapid_exponentiation_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math"
+	"testing"
+)
+
+func Test_rapidExponentiation(t *testing.T) {
+	a, n := 2, 10
+	expected := 1024
+	res := rapidExponentiation(a, n)
+	assert.Equal(t, expected, res)
+}
+
+func Test_rapidExponentiation2(t *testing.T) {
+	a, n := 2, 10
+	expected := 1024
+	res := rapidExponentiation2(a, n)
+	assert.Equal(t, expected, res)
+}
+
+func Test_pow(t *testing.T) {
+	a, n := 2, 10
+	expected := 1024
+	res := int(math.Pow(float64(a), float64(n)))
+	assert.Equal(t, expected, res)
+}
+
+func Benchmark_rapidExponentiation(b *testing.B) {
+	a, n := 2, 36
+	for i := 0; i < b.N; i++ {
+		rapidExponentiation(a, n)
+	}
+}
+
+func Benchmark_rapidExponentiation2(b *testing.B) {
+	a, n := 2, 36
+	for i := 0; i < b.N; i++ {
+		rapidExponentiation2(a, n)
+	}
+}
+
+func Benchmark_pow(b *testing.B) {
+	a, n := 2, 36
+	for i := 0; i < b.N; i++ {
+		_ = int(math.Pow(float64(a), float64(n)))
+	}
+}
+
+/*
+goos: linux
+goarch: amd64
+pkg: leetcode/data_algorithms/rapid_exponentiation
+cpu: Intel(R) Core(TM) i3-10100 CPU @ 3.60GHz
+Benchmark_rapidExponentiation-8    	100000000	        11.31 ns/op	       0 B/op	       0 allocs/op
+Benchmark_rapidExponentiation2-8   	100000000	        10.79 ns/op	       0 B/op	       0 allocs/op
+Benchmark_pow-8                    	55795579	        21.42 ns/op	       0 B/op	       0 allocs/op
+*/
