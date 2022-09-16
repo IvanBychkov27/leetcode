aa4414ee2378c4b32002c67e44ffde64f0ca47ee
Ivan <IvanBychkov@mail.ru>
2022-09-16 13:01:13 +0300

    Add: динамическое программирование вычисление числа фибоначчи

--- data_algorithms/fibonacci_numbers/main.go
+++ data_algorithms/fibonacci_numbers/main.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	//n := 20
-	//numsFib := numbersFibonacciInArray(n)
-	//fmt.Println("numbers Fibonacci:", numsFib)
+	n := 40
+	numbersFibonacciLine(n)
+	fmt.Printf("\n\n")
+
+	resR := fibRecursive(n)
+	fmt.Printf("resR = %d\n\n", resR)
+
+	resD := fibDynamicArray(n)
+	fmt.Printf("resD = %d\n\n", resD)
+
+	resABC := fibDynamicABC(n)
+	fmt.Printf("resABC = %d\n\n", resABC)
+
+	numsFib := numbersFibonacciInArray(n)
+	fmt.Println("numbers Fibonacci:", numsFib)
 
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21}
 	target := 12
@@ -44,6 +56,7 @@ func searchFibonacci(nums []int, target int) bool {
 	}
 }
 
+// динамическое программирование
 func numbersFibonacciLine(n int) {
 	var a, b int
 	for i := 0; i < n; i++ {
@@ -57,6 +70,7 @@ func numbersFibonacciLine(n int) {
 	}
 }
 
+// динамическое программирование
 func numbersFibonacciInArray(n int) []int {
 	if n < 1 {
 		return []int{}
@@ -76,4 +90,36 @@ func numbersFibonacciInArray(n int) []int {
 	}
 
 	return nums
+}
+
+// Рекурсия работает долго!!! - время работы растет прямо пропорционально числам фибоначчи (введите например n=45 и более)
+// Сложность O(fib(n))
+func fibRecursive(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fibRecursive(n-1) + fibRecursive(n-2)
+}
+
+// динамическое программирование вычисление числа фибоначчи
+// Сложность O(n)
+func fibDynamicArray(n int) int {
+	fib := make([]int, n+1)
+	fib[0] = 0
+	fib[1] = 1
+	for i := 2; i <= n; i++ {
+		fib[i] = fib[i-1] + fib[i-2]
+	}
+	return fib[n]
+}
+
+func fibDynamicABC(n int) int {
+	var a, b, c int
+	b = 1
+	for i := 2; i <= n; i++ {
+		c = a + b
+		a = b
+		b = c
+	}
+	return c
 }
